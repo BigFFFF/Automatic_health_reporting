33c8ee4138ca5949c32484d067c0ec0e619bfb27
BlowFFF <e762938604@gmail.com>
2022-05-01 18:26:39 +0800

    use selenium4

--- loginwzjw.py
+++ loginwzjw.py
@@ -1,66 +1,49 @@
 from selenium import webdriver
-from time import sleep
-from PIL import Image 
-from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support import expected_conditions as EC
 import muggle_ocr
 
 
 class Login():
     
-    def __init__(self, usern, passw, url, webdri_path):
+    def __init__(self, usern, passw, url):
         self.usern = usern
         self.passw = passw
-        self.url = url
-        self.webdri_path = webdri_path
+        self.url = url  
 
-    #处理验证码图片
-    def cut_code(self, e):
-        left = int(e.location['x'])
-        top = int(e.location['y'])
-        right = int(e.location['x'] + e.size['width'])
-        bottom = int(e.location['y'] + e.size['height'])
-
-        im = Image.open('imgs/screenshot.png')
-        im = im.crop((left, top, right, bottom))
-        im.save('imgs/screenshot.png')
-
-    #识别验证码图片
-    def ocr_code(self, image):
-        sdk = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.Captcha)
-
-        with open(image, "rb") as f:
-            captcha_bytes = f.read()
-            
-        tword = sdk.predict(image_bytes=captcha_bytes)
+    #获取验证码图片并识别
+    def ocr_code(self, e):
+        img_as_bytes = e.screenshot_as_png
+        ocr = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.Captcha)
+        tword = ocr.predict(img_as_bytes)
         return tword
     
     def main(self):
-        
-        driver = webdriver.Edge(webdri_path)
+        options = Options()
+        options.use_chromium = True
+        options.headless
+        #浏览器自动关闭关
+        options.add_experimental_option("detach", True)
+        options.add_experimental_option('excludeSwitches', ['enable-automation'])
+        driver = webdriver.Edge(options = options)
+        #隐式等待开(s)
+        driver.implicitly_wait(10)
         driver.get(url)
-        driver.maximize_window()
-        sleep(1)
 
         while True:    
             #输入用户名和密码
-            driver.find_element_by_name('username').send_keys(usern)
-            driver.find_element_by_name('password').send_keys(passw)
+            driver.find_element(By.ID, 'username').send_keys(usern)
+            driver.find_element(By.ID, 'password').send_keys(passw)
 
             #获取与填写验证码
             driver.get_screenshot_as_file(u'imgs/screenshot.png')
-            elem = driver.find_element_by_xpath('//*[@id="captchaAccount"]')
-            self.cut_code(elem)
-            
-            image = r"./imgs/screenshot.png"
-            tw = self.ocr_code(image)
-            sleep(1)
+            elem = driver.find_element(By.ID, 'captchaAccount')
 
-            driver.find_element_by_id('captcha').send_keys(tw)
-            sleep(1)
+            tw = self.ocr_code(elem)
 
-            driver.find_element_by_id('captcha').send_keys(Keys.ENTER)
-            sleep(1)
+            driver.find_element(By.ID, 'captcha').send_keys(tw + Keys.ENTER)
             
             #判定是否登录成功
             if(driver.current_url == "http://my.sdwz.cn/uc/user/index"):
@@ -71,25 +54,21 @@ class Login():
         now_handle = driver.current_window_handle
         driver.switch_to.window(now_handle)
 
-        driver.find_element_by_link_text("每日健康上报").click()
-        sleep(1)
-
-        handles = driver.window_handles
-
         #句柄切换至新页面
+        driver.find_element(By.LINK_TEXT, "每日健康上报").click()
+        handles = driver.window_handles
         for handle in handles:
-            if handle!=now_handle:
+            if handle != now_handle:
                 driver.switch_to.window(handle)
+                break
                 
+        #点击单选框
+        driver.find_element(By.XPATH, "//*[@id=\"SFHSCY2_ct\"]/label[1]/nobr/input").click()
+        driver.find_element(By.XPATH, "//*[@id=\"SKMYS2_ct\"]/label[1]/nobr/input").click()      
+        driver.find_element(By.XPATH, "//*[@id=\"GTJZQK2_ct\"]/label[1]/nobr/input").click()
 
-        driver.find_element_by_xpath("//*[@id=\"SKMYS2_ct\"]/label[1]/nobr/input").click()
-        sleep(1)
-
-        driver.find_element_by_id("ext-gen25").click()
-        sleep(1)
-
-        driver.find_element_by_id("ext-gen27").click()
-        sleep(1)
+        driver.find_element(By.ID, "ext-gen25").click()
+        driver.find_element(By.ID, "ext-gen27").click()
 
         driver.quit()
 
@@ -100,12 +79,8 @@ if __name__ == "__main__":
     usern = ''
     passw = ''
     url = 'http://sso.sdwz.cn/cas/login?service=http%3A%2F%2Fmy.sdwz.cn%2Flogin'
-    webdri_path = 'G:/Python/msedgedriver.exe'
 
-    run = Login(usern,
-                passw,
-                url,
-                webdri_path)
+    run = Login(usern, passw, url)
 
     run.main()
 
